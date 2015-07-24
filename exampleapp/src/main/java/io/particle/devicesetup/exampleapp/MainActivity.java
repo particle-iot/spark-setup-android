4ab21fc1f9f5ee937c2164e6c74960191565850a
Jens Knutson <jens.knutson@gmail.com>
2015-07-24 18:56:22 -0500

    Add a button to the example activity to trigger setup

--- exampleapp/src/main/java/io/particle/devicesetup/exampleapp/MainActivity.java
+++ exampleapp/src/main/java/io/particle/devicesetup/exampleapp/MainActivity.java
@@ -1,12 +1,11 @@
 package io.particle.devicesetup.exampleapp;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import io.particle.android.sdk.devicesetup.ParticleDeviceSetupLibrary;
+import io.particle.android.sdk.utils.ui.Ui;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,33 +15,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ParticleDeviceSetupLibrary.init(this.getApplicationContext(), MainActivity.class);
 
-        invokeDeviceSetup();
+        Ui.findView(this, R.id.start_setup_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                invokeDeviceSetup();
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void invokeDeviceSetup()
-    {
+    public void invokeDeviceSetup() {
         ParticleDeviceSetupLibrary.startDeviceSetup(this);
     }
 }
