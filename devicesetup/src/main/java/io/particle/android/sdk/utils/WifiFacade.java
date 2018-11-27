ea1d25b33125416a2fc3d7e8e13bf84d3be133ab
Jens Knutson <jens.knutson@gmail.com>
2018-11-27 16:18:19 -0600

    Fix Photon setup for recent Samsung devices

--- devicesetup/src/main/java/io/particle/android/sdk/utils/WifiFacade.java
+++ devicesetup/src/main/java/io/particle/android/sdk/utils/WifiFacade.java
@@ -112,11 +112,16 @@ public class WifiFacade {
         // "I want the Network obj for the Wi-Fi network with SSID <foo>".
         // Instead, you have to infer it based on the fact that you can only
         // have one connected Wi-Fi connection at a time.
+        // (Update: one *regular* Wi-Fi connection, anyway.  See below.)
+
         return Funcy.findFirstMatch(
                 Arrays.asList(connectivityManager.getAllNetworks()),
                 network -> {
                     NetworkCapabilities capabilities = connectivityManager.getNetworkCapabilities(network);
-                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI);
+                    // Don't try using the P2P Wi-Fi interfaces on recent Samsung devices
+                    if (capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_WIFI_P2P)) {
+                        return false;
+                    }
                     return capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI);
                 }
         );
